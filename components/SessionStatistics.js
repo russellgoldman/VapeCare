2604ae6e60947e2c610eed76e0d4ec5ccd408fe1
russellgoldman <russellgoldman1@gmail.com>
2019-09-15 03:15:02 -0400

    SessionStatistics

--- components/SessionStatistics.js
+++ components/SessionStatistics.js
@@ -1,51 +1,83 @@
-import React, { useState } from 'react'
-import { View, Text } from 'react-native'
-
-import Swiper from 'react-native-swiper';
+import React, { Component } from 'react'
+import { View, Text, Image } from 'react-native'
 
 import layoutConstants from '../constants/Layout'
 const { statusBarHeight, calcWidth, calcHeight } = layoutConstants
 
 import colors from '../constants/Colors'
-const { white, black, supportGrey } = colors
+const { white, black, supportGrey, blue } = colors
 
-export default SessionStatistics = () => {
-    const { swiperContainer, swipeContent } = styles
+export default class SessionStatistics extends Component {
+    async getMoviesFromApiAsync() {
+        return fetch('http://35.203.112.83:5000/vape/weekly/avg-duration')
+        .then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson)
+            return responseJson
+        })
+        .catch((error) => {
+        console.error(error);
+        });
+    }
 
-    const [dayNum, setDayNum] = useState(1)
+    componentWillMount() {
+        this.getMoviesFromApiAsync()
+    }
 
-    return (
-        <View>
-            <View style={{ flexDirection: 'row' }}>
-                <Text style={{
-                    paddingLeft: calcWidth(48),
-                    fontFamily: 'sf-bold',
-                    fontSize: 15,
-                    color: black,
-                    letterSpacing: 2.5
-                }}>WEEKLY STATISTICS</Text>
-                <Text style={{
-                    paddingLeft: calcWidth(39),
-                    fontFamily: 'sf-bold',
-                    fontSize: 15,
-                    color: supportGrey,
-                    letterSpacing: 2.5
-                }}>DAY {dayNum}</Text>
-            </View>
-            <Swiper style={swiperContainer} showsButtons={true}>
-                <View style={swipeContent}>
+    constructor() {
+        super()
+        state = {
+            weeklySessions: 0
+        }
+    }
 
+    render() {
+        const { sessionContainer } = styles
+        return (
+            <>
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={{
+                        paddingLeft: calcWidth(48),
+                        fontFamily: 'sf-bold',
+                        fontSize: 15,
+                        color: black,
+                        letterSpacing: 2.5
+                    }}>WEEKLY STATISTICS</Text>
+                    <Text style={{
+                        paddingLeft: calcWidth(39),
+                        fontFamily: 'sf-bold',
+                        fontSize: 15,
+                        color: supportGrey,
+                        letterSpacing: 2.5
+                    }}>DAY 1</Text>
                 </View>
-            </Swiper>
-        </View>
-    )
+                <View style={sessionContainer}>
+                    <View>
+                        <Text>Average Sessions</Text>
+                        <Text></Text>
+                    </View>
+                    <View></View>
+                </View>
+            </>
+        )
+    }
 }
 
 const styles = {
-    swiperContainer: {
-
-    },
-    swipeContent: {
+    sessionContainer: {
+        flexDirection: 'row',
         height: calcHeight(116),
+        width: calcWidth(319),
+        marginTop: calcHeight(14),
+        marginLeft: calcWidth(28),
+        backgroundColor: blue,
+        borderRadius: 20,
+        shadowColor: black,
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+        shadowOffset: {
+            width: 4,
+            height: 4
+        },
     }
 }
--- constants/Colors.js
+++ constants/Colors.js
@@ -2,4 +2,5 @@ export default {
   white: '#fff',
   black: '#000',
   supportGrey: '#979797',
+  blue: '#0AB8CF'
 };
